c1ca8e0cfc7ce3365bc1fd836f32e16b145026f7
Gaurav Makhijani <makhijani.gaurav19@gmail.com>
2021-10-08 16:19:53 +0530

    added show-now-cont

---
+++ src/components/home/HeroImage.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default class HeroImage extends Component {
+  render() {
+    return (
+      <Container className="cont-margin">
+        <Row className="row">
+          <Col>
+            <h3 className="hero-img-h3">VEGGIE SUPPER MARKET</h3>
+            <h1 className="hero-img-h1">it’s Organic</h1>
+            <h6 className="hero-img-h6">
+              The organic veggies and fruits from worlds finest frams
+            </h6>
+          </Col>
+          <Col>
+            <img
+              className="hero-banner"
+              src="http://wp.0effortthemes.com/themes/veggie/wp-content/uploads/2016/08/Untitled-10-min.png"
+              alt="banner"
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,28 +1,43 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-
+import { Container, Row, Col, Button } from "react-bootstrap";
+import HeroImage from "./HeroImage";
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <Container className="cont-margin">
-          <Row className="hero-img-row">
-            <Col>
-              <h3 className="hero-img-h3">VEGGIE SUPPER MARKET</h3>
-              <h1 className="hero-img-h1">it’s Organic</h1>
-              <h6 className="hero-img-h6">
-                The organic veggies and fruits from worlds finest frams
-              </h6>
-            </Col>
-            <Col>
-              <img
-                className="hero-banner"
-                src="http://wp.0effortthemes.com/themes/veggie/wp-content/uploads/2016/08/Untitled-10-min.png"
-                alt="banner"
-              />
-            </Col>
-          </Row>
-        </Container>
+        <HeroImage />
+        <div className="cont-margin shop-now-cont">
+          <Container className="cont-pd">
+            <Row className="row ">
+              <Col>
+                <img
+                  className=""
+                  src="http://wp.0effortthemes.com/themes/veggie/wp-content/uploads/2016/02/veg-rack.png"
+                  alt="rack"
+                />
+              </Col>
+              <Col className="p-6">
+                <h1 className="shop-now-h1">Why veggie super market?</h1>
+                <p className="shop-now-p mt-4">
+                  We shorten the time between the farm and your table. Our motto
+                  is to deliver the local food which is better, fresher and
+                  tastier. The wholesale prices that farmers get for their
+                  products are low, often near the cost of production. It is an
+                  investment in the future. By supporting local farmers today,
+                  we are helping to ensure that there will be farms in our
+                  community tomorrow!
+                </p>
+                <p className="shop-now-p">
+                  We have 6 points quality control system that ensures that each
+                  piece of fruits and vegetables should match Grade A Quality.
+                  Our Logistics chain ensure each fruits and vegetables reaches
+                  your doorsteps within 24 hours of plucking and harvesting.
+                </p>
+                <Button className="shop-now-button mt-4">PURCHASE NOW</Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     );
   }
